e636f865e159ec12b81a0e12e73aff3f1028a708
m-krol-github <88250519+m-krol-github@users.noreply.github.com>
2022-11-07 12:01:05 +0100

    basic scene loader

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,9 +1,28 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MainMenu
 {
     public class MainMenuManager : MonoBehaviour
     {
-               
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button exitGameButton;
+        [SerializeField] private TMP_InputField playerNameField;
+
+        private void Awake()
+        {
+            playButton.onClick.AddListener(() =>
+            {
+                SceneLoadManager.Instance.LoadScene(SceneLoadManager.SCENE_GAMEPLAY);
+            });
+            
+            exitGameButton.onClick.AddListener(() =>
+            {
+                SceneLoadManager.Instance.ExitGame();
+            });
+        }
+        
+            
     }
 }
---
+++ Assets/Scripts/Utils/SceneLoadManager.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MainMenu
+{
+    public class SceneLoadManager : Singleton<SceneLoadManager>
+    {
+        public static readonly string SCENE_MAINMENU = "MainMenu";
+
+        public static readonly string SCENE_GAMEPLAY = "WorkScene"; //todo: rename scene to "Gameplay" when ready to realase
+
+        public static readonly string SCENE_EXITGAME = "GameOver";
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
